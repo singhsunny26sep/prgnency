00a7fd1ca6b5ee2a56337e86ee5f1a51720af838
kishan-vaghasiya <vaghasiyakishan030@gmail.com>
2025-09-15 08:35:49 +0600

    update pacage name

--- android/app/src/main/java/com/dreamchild/MainActivity.kt
+++ android/app/src/main/java/com/dreamchild/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dreamchild
+package com.hiranyagarbh
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/dreamchild/MainApplication.kt
+++ android/app/src/main/java/com/dreamchild/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dreamchild
+package com.hiranyagarbh
 
 import android.app.Application
 import com.facebook.react.PackageList
